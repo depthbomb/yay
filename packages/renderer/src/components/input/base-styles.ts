ed3862886aaec3d8909703cdaa38b7c05ae965ba
depthbomb <depthbomb@users.noreply.github.com>
2025-03-08 02:35:01 -0600

    refactor: adjust styling of inputs

--- packages/renderer/src/components/input/base-styles.ts
+++ packages/renderer/src/components/input/base-styles.ts
@@ -1,10 +1,11 @@
 import clsx from 'clsx';
 
 export const baseCss = clsx([
-	'py-1 px-3 text-lg bg-gray-700 rounded',
+	'py-1 px-3 text-lg bg-gray-700 border border-gray-600 rounded',
 	'placeholder:text-gray-400',
 	'hover:placeholder:text-gray-300',
 	'hover:bg-gray-600',
 	'focus:placeholder:text-gray-300',
-	'focus:bg-gray-700'
+	'focus:bg-gray-700',
+	'focus:border-brand-600'
 ]);
