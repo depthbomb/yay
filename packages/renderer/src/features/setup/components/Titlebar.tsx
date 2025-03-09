b5c3cb77bc0a7c30afc920d85f2706fc405483b8
depthbomb <depthbomb@users.noreply.github.com>
2025-03-08 21:36:09 -0600

    refactor: make titlebar icon more draggable

--- packages/renderer/src/features/setup/components/Titlebar.tsx
+++ packages/renderer/src/features/setup/components/Titlebar.tsx
@@ -1,6 +1,6 @@
 import logo from '~/assets/img/logo.svg';
 import { TitlebarButton } from './TitlebarButton';
-import { FC } from 'react';
+import type { FC } from 'react';
 
 type TitlebarProps = {
 	title?: string;
@@ -9,8 +9,12 @@ type TitlebarProps = {
 
 export const Titlebar: FC<TitlebarProps> = ({ title, showIcon = true }) => {
 	return (
-		<div className="absolute top-0 pt-[1px] pr-[1px] pl-3 w-full h-8 flex items-center">
-			{showIcon && <img src={logo} className="mr-2 w-4 h-8" draggable="false"/>}
+		<div className="absolute top-0 pt-[1px] pr-[1px] w-full h-8 flex items-center">
+			{showIcon && (
+				<div className="flex items-center justify-center w-12 h-8 draggable">
+					<img src={logo} className="size-4" width="16" height="16" draggable="false"/>
+				</div>
+			)}
 			{title && <p className="min-w-max h-8 leading-8 text-xs draggable">{title}</p>}
 			<span className="w-full h-full draggable"/>
 			<TitlebarButton onClick={() => window.api.minimizeWindow('setup')} type="minimize"/>
