c781391b83f5c75f05fb2630775f2bf40189772d
depthbomb <depthbomb@users.noreply.github.com>
2025-03-09 03:53:05 -0500

    refactor: better convey ToggleButton info

--- packages/renderer/src/components/ToggleButton.tsx
+++ packages/renderer/src/components/ToggleButton.tsx
@@ -14,8 +14,8 @@ export const ToggleButton = forwardRef<HTMLButtonElement, ToggleButtonProps>(({ 
 		'flex flex-row items-center justify-center px-1.5 space-x-1 min-w-16 h-6 text-xs rounded',
 		{
 			'text-white bg-orange-500 hover:bg-orange-600 active:bg-orange-700': isInitial,
-			'text-lime-950 bg-lime-500 hover:bg-lime-600 active:bg-lime-700': !enabled,
-			'bg-red-500 hover:bg-red-600 active:bg-red-700': enabled,
+			'text-lime-950 bg-lime-500 hover:bg-lime-600 active:bg-lime-700': enabled,
+			'bg-red-500 hover:bg-red-600 active:bg-red-700': !enabled,
 		},
 		className
 	);
@@ -24,8 +24,8 @@ export const ToggleButton = forwardRef<HTMLButtonElement, ToggleButtonProps>(({ 
 
 	return (
 		<button ref={ref} className={css} onClick={onClick} {...props} type="button">
-			<Icon path={isInitial ? mdiMinus : enabled ? mdiClose : mdiCheck} className="size-4"/>
-			<span>{ isInitial ? '...' : enabled ? 'Disable' : 'Enable' }</span>
+			<Icon path={isInitial ? mdiMinus : enabled ? mdiCheck : mdiClose} className="size-4"/>
+			<span>{ isInitial ? '...' : enabled ? 'Enabled' : 'Disabled' }</span>
 		</button>
 	);
 });
