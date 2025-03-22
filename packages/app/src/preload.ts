d71b559054b25ae35ff82c55bff4df5918e1df52
depthbomb <depthbomb@users.noreply.github.com>
2025-03-22 14:19:55 -0500

    refactor: attempt to improve exposed API typing

--- packages/app/src/preload.ts
+++ packages/app/src/preload.ts
@@ -2,66 +2,65 @@ import { ipcRenderer, contextBridge } from 'electron';
 import { IpcChannel, IpcChannels, SettingsKey } from 'shared';
 import { arch, type, release, platform, hostname } from 'node:os';
 import type { IpcRendererEvent, MessageBoxOptions } from 'electron';
+import type { IpcApi, CoreApi, SystemApi, VersionsApi, SettingsApi } from 'shared';
 
-function on(channel: IpcChannel, listener: (...args: any[]) => void) {
-	if (!IpcChannels.includes(channel)) {
-		if (__STRICT__) {
-			throw new Error(`Attempted to listen to invalid channel: ${channel}`);
+const versionsApi = process.versions satisfies VersionsApi;
+
+const ipcApi = {
+	on(channel: IpcChannel, listener: (...args: any[]) => void) {
+		if (!IpcChannels.includes(channel)) {
+			if (__STRICT__) {
+				throw new Error(`Attempted to listen to invalid channel: ${channel}`);
+			}
+
+			console.error('Attempted to listen to invalid channel', { channel });
+			return () => {};
 		}
 
-		console.error('Attempted to listen to invalid channel', { channel });
-		return () => {};
-	}
+		const cb = (_: IpcRendererEvent, ...args: any[]) => listener(...args);
 
-	const cb = (_: IpcRendererEvent, ...args: any[]) => listener(...args);
+		ipcRenderer.on(channel, cb);
+		return () => ipcRenderer.removeListener(channel, cb);
+	},
+	once(channel: IpcChannel, listener: (...args: any[]) => void) {
+		if (!IpcChannels.includes(channel)) {
+			if (__STRICT__) {
+				throw new Error(`Attempted to listen to invalid channel: ${channel}`);
+			}
 
-	ipcRenderer.on(channel, cb);
-	return () => ipcRenderer.removeListener(channel, cb);
-}
-
-function once(channel: IpcChannel, listener: (...args: any[]) => void) {
-	if (!IpcChannels.includes(channel)) {
-		if (__STRICT__) {
-			throw new Error(`Attempted to listen to invalid channel: ${channel}`);
+			console.error('Attempted to listen to invalid channel', { channel });
+			return;
 		}
 
-		console.error('Attempted to listen to invalid channel', { channel });
-		return;
-	}
+		ipcRenderer.once(channel, (_, ...args) => listener(...args));
+	},
+	off (channel: IpcChannel, listener: (...args: any[]) => void) {
+		if (!IpcChannels.includes(channel)) {
+			if (__STRICT__) {
+				throw new Error(`Attempted to remove listener from invalid channel: ${channel}`);
+			}
 
-	ipcRenderer.once(channel, (_, ...args) => listener(...args));
-}
-
-function off (channel: IpcChannel, listener: (...args: any[]) => void) {
-	if (!IpcChannels.includes(channel)) {
-		if (__STRICT__) {
-			throw new Error(`Attempted to remove listener from invalid channel: ${channel}`);
+			console.error('Attempted to remove listener from invalid channel', { channel });
+			return;
 		}
 
-		console.error('Attempted to remove listener from invalid channel', { channel });
-		return;
-	}
+		ipcRenderer.removeListener(channel, listener);
+	},
+	removeAllListeners (channel: IpcChannel) {
+		if (!IpcChannels.includes(channel)) {
+			if (__STRICT__) {
+				throw new Error(`Attempted to remove listeners from invalid channel: ${channel}`);
+			}
 
-	ipcRenderer.removeListener(channel, listener);
-}
-
-function removeAllListeners (channel: IpcChannel) {
-	if (!IpcChannels.includes(channel)) {
-		if (__STRICT__) {
-			throw new Error(`Attempted to remove listeners from invalid channel: ${channel}`);
+			console.error('Attempted to remove listeners from invalid channel', { channel });
+			return;
 		}
 
-		console.error('Attempted to remove listeners from invalid channel', { channel });
-		return;
+		ipcRenderer.removeAllListeners(channel);
 	}
+} satisfies IpcApi;
 
-	ipcRenderer.removeAllListeners(channel);
-}
-
-contextBridge.exposeInMainWorld('versions', process.versions);
-contextBridge.exposeInMainWorld('buildDate', new Date(__BUILD_DATE__));
-contextBridge.exposeInMainWorld('ipc', { on, once, off, removeAllListeners });
-contextBridge.exposeInMainWorld('api', {
+const coreApi = {
 	minimizeWindow(windowName: string) {
 		return ipcRenderer.invoke(IpcChannel.MinimizeWindow, windowName);
 	},
@@ -145,8 +144,17 @@ contextBridge.exposeInMainWorld('api', {
 	toggleGlobalMenu() {
 		return ipcRenderer.invoke(IpcChannel.ToggleGlobalMenu);
 	},
-});
-contextBridge.exposeInMainWorld('system', { arch, type, release, platform, hostname });
-contextBridge.exposeInMainWorld('settings', {
-	getValue: (key: string, defaultValue?: unknown, secure?: boolean) => ipcRenderer.sendSync(IpcChannel.GetSettingsValue, key, defaultValue, secure)
-});
+} satisfies CoreApi;
+
+const systemApi = { arch, type, release, platform, hostname } satisfies SystemApi;
+
+const settingsApi = {
+	getValue: (key: SettingsKey, defaultValue?: unknown, secure?: boolean) => ipcRenderer.sendSync(IpcChannel.GetSettingsValue, key, defaultValue, secure)
+} satisfies SettingsApi;
+
+contextBridge.exposeInMainWorld('versions', versionsApi);
+contextBridge.exposeInMainWorld('buildDate', new Date(__BUILD_DATE__));
+contextBridge.exposeInMainWorld('ipc', ipcApi);
+contextBridge.exposeInMainWorld('api', coreApi);
+contextBridge.exposeInMainWorld('system', systemApi);
+contextBridge.exposeInMainWorld('settings', settingsApi);
--- packages/shared/src/types.ts
+++ packages/shared/src/types.ts
@@ -1,3 +1,62 @@
+import type { IpcChannel } from './ipc';
+import type { SettingsKey } from './settings';
+import type { MessageBoxOptions, MessageBoxReturnValue } from 'electron';
+
 export type Awaitable<T> = PromiseLike<T> | T;
 export type Maybe<T>     = T | undefined;
 export type Nullable<T>  = T | null;
+
+export type VersionsApi = typeof process.versions;
+
+export type IpcApi = {
+	on: (channel: IpcChannel, listener: (...args: any[]) => void) => () => void;
+	once: (channel: IpcChannel, listener: (...args: any[]) => void) => void;
+	off: (channel: IpcChannel, listener: (...args: any[]) => void) => void;
+	removeAllListeners: (channel: IpcChannel) => void;
+};
+
+export type CoreApi = {
+	minimizeWindow(windowName: string): Promise<void>;
+	playNotificationSound(): Promise<void>;
+	showInputRightClickMenu(): Promise<void>;
+	openDownloadDir(): Promise<void>;
+	openDownloadDirPicker(): Promise<void>;
+	toggleWindowPinned(): Promise<boolean>;
+	showMessageBox(options: MessageBoxOptions): Promise<MessageBoxReturnValue>;
+	//
+	cancelSetup(): Promise<void>;
+	//
+	getSettingsValue<T>(key: SettingsKey, defaultValue?: any, secure?: boolean): Promise<T>;
+	setSettingsValue(key: SettingsKey, value: any, secure?: boolean): Promise<void>;
+	resetSettings(): Promise<void>;
+	//
+	downloadVideo(url: string): Promise<void>;
+	downloadAudio(url: string): Promise<void>;
+	downloadDefault(url: string): Promise<void>;
+	cancelDownload(): Promise<void>;
+	recheckBinaries(): Promise<void>;
+	checkForYtdlpUpdate(): Promise<boolean>;
+	updateYtdlpBinary(): Promise<void>;
+	//
+	getAutoStart(): Promise<boolean>;
+	enableAutoStart(): Promise<void>;
+	disableAutoStart(): Promise<void>;
+	toggleAutoStart(): Promise<boolean>;
+	//
+	getGlobalMenuEnabled(): Promise<boolean>;
+	enableGlobalMenu(): Promise<void>;
+	disableGlobalMenu(): Promise<void>;
+	toggleGlobalMenu(): Promise<boolean>;
+};
+
+export type SystemApi = {
+	arch: () => string;
+	type: () => string;
+	release: () => string;
+	platform: () => NodeJS.Platform;
+	hostname: () => string;
+};
+
+export type SettingsApi = {
+	getValue: <T>(key: SettingsKey, defaultValue?: unknown, secure?: boolean) => T
+};
