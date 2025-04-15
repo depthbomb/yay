dd708a8c5801c4af6757df8bc3607879217af5dd
depthbomb <depthbomb@users.noreply.github.com>
2025-04-15 18:38:31 -0500

    refactor: use `SettingsKey` instead of `string` for `settings-event` event handler args

--- packages/app/src/lib/EventEmitter/types/Events.ts
+++ packages/app/src/lib/EventEmitter/types/Events.ts
@@ -1,8 +1,9 @@
+import type { SettingsKey } from 'shared';
 import type { BrowserWindow } from 'electron';
 
 export type Events = {
 	'setup-finished': void;
-	'settings-updated': { key: string, value: unknown };
+	'settings-updated': { key: SettingsKey, value: unknown };
 	'download-started': string;
 	'download-finished': void;
 	'window-created': BrowserWindow;
