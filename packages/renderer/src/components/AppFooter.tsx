7706286973e63f5237fd6c1b407d775d5a9d72d9
depthbomb <depthbomb@users.noreply.github.com>
2025-04-17 23:34:50 -0500

    fix: app footer hint not using `Anchor` component

--- packages/renderer/src/components/AppFooter.tsx
+++ packages/renderer/src/components/AppFooter.tsx
@@ -1,4 +1,5 @@
 import Icon from '@mdi/react';
+import { Anchor } from './Anchor';
 import { mdiCreation } from '@mdi/js';
 import { useEffect, useState } from 'react';
 import { KeyCombo } from '~/components/KeyCombo';
@@ -8,7 +9,7 @@ import type { Maybe } from 'shared';
 const hints = [
 	<span className="text-xs">Press <KeyCombo keys={['ctrl', 'v']}/> with the menu open to paste a valid URL!</span>,
 	<span className="text-xs">Press <KeyCombo keys={['enter']}/> after pasting to start the download!</span>,
-	<span className="text-xs"><strong>yay</strong> can download from <a href="https://github.com/yt-dlp/yt-dlp/blob/master/supportedsites.md#supported-sites" target="_blank">many sites</a>, not just YouTube!</span>,
+	<span className="text-xs"><strong>yay</strong> can download from <Anchor href="https://github.com/yt-dlp/yt-dlp/blob/master/supportedsites.md#supported-sites" target="_blank">many sites</Anchor>, not just YouTube!</span>,
 ] as const;
 
 export const AppFooter = () => {
@@ -27,7 +28,7 @@ export const AppFooter = () => {
 	}, []);
 
 	return (
-		<footer className="space-x-2 px-3 pb-3 flex items-center justify-center shrink-0 text-gray-300">
+		<footer className="space-x-2 pb-3 h-7 flex items-center justify-center shrink-0 text-gray-300">
 			<Icon path={mdiCreation} className="size-4 shrink-0 text-yellow-500"/>
 			<p className="flex items-center">{hint}</p>
 		</footer>
