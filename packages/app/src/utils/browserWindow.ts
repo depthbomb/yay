c4ea67e419d8b87b1e200bce5f2d7fd5fdb1f7e0
depthbomb <depthbomb@users.noreply.github.com>
2025-04-19 16:23:28 -0500

    refactor: whitelist additional hosts

--- packages/app/src/utils/browserWindow.ts
+++ packages/app/src/utils/browserWindow.ts
@@ -3,10 +3,7 @@ import type { HandlerDetails, WindowOpenHandlerResponse } from 'electron';
 
 export function windowOpenHandler({ url }: HandlerDetails): WindowOpenHandlerResponse {
 	const requestedUrl = new URL(url);
-	if (requestedUrl.host === 'github.com') {
-		/**
-		 * Currently the only intended links to open in an external browser are for GitHub.
-		 */
+	if (requestedUrl.host === 'github.com' || requestedUrl.host.endsWith('electronjs.org')) {
 		shell.openExternal(url);
 	}
 
