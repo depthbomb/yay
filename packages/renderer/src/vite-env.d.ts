c807d147018425b53a997436e6ee90a16523542b
depthbomb <depthbomb@users.noreply.github.com>
2026-01-27 17:00:55 -0600

    chore: move global window interface definition to root declaration

--- packages/renderer/src/vite-env.d.ts
+++ packages/renderer/src/vite-env.d.ts
@@ -1,1 +1,14 @@
 /// <reference types="vite/client" />
+
+import type { IPCAPI, SystemAPI, VersionsAPI, SettingsAPI, FeatureFlagsAPI } from 'shared';
+
+declare global {
+	interface Window {
+		buildDate: Date;
+		versions: VersionsAPI;
+		ipc: IPCAPI;
+		system: SystemAPI;
+		settings: SettingsAPI;
+		featureFlags: FeatureFlagsAPI;
+	}
+}
